2bfb946027834f4f4e3e65221e6c70ee8861bbb6
qysp <qysp.yo@gmail.com>
2020-09-22 09:34:47 +0200

    Enhance CSS styling and refactor

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# showoff
+showoff
+=======
 
 Simple markdown presentations in the browser.
 
-## Usage
+Usage
+-----
 
 1. Create slides
 2. Start the server
 3. Visit http://localhost:3000 (if used with default configurations)
 4. Click a slide or use your *arrow keys* to navigate through the slides and *escape* to go back to the ToC.
 
-## Slides format
+Slides format
+-------------
 
 When creating slides use the format: `{SLIDE_POSITION}__{SLIDE_TITLE}.md`.
 
@@ -17,6 +20,7 @@ An example would be: `01__My awesome slide.md`.
 
 Note the double *underscores* after the slide position.
 
-## Why?
+Why?
+----
 
 I like alpine.
--- server/app.mjs
+++ server/app.mjs
@@ -1,12 +1,13 @@
-import { promises as fs } from 'fs';
+import { readFile, readdir } from 'fs/promises';
 import { join } from 'path';
 import marked from 'marked';
 import Koa from 'koa';
 import Router from '@koa/router';
 import serve from 'koa-static';
 
-const CWD = process.cwd();
-const SLIDES_DIR = join(CWD, 'slides');
+const ROOT = process.cwd();
+const NODE_MODULES = join(ROOT, 'node_modules');
+const SLIDES_DIR = join(ROOT, 'slides');
 
 const app = new Koa();
 
@@ -16,34 +17,35 @@ const router = new Router()
 
 app
   .use(router.routes())
-  .use(serve(join(CWD, 'public'), { index: 'index.html' }))
-  .use(serve(join(CWD, 'node_modules', 'milligram', 'dist')))
-  .use(serve(join(CWD, 'node_modules', 'normalize.css')))
-  .use(serve(join(CWD, 'node_modules', 'alpinejs', 'dist')))
+  .use(serve(join(ROOT, 'public'), { index: 'index.html' }))
+  .use(serve(join(NODE_MODULES, 'milligram', 'dist')))
+  .use(serve(join(NODE_MODULES, 'normalize.css')))
+  .use(serve(join(NODE_MODULES, 'alpinejs', 'dist')))
   .listen(parseInt(process.env.PORT || 3000, 10));
 
 /**
  * Respond with available slides.
- * @param {Koa.Context} ctx 
+ * @param {Koa.Context} ctx
  */
 async function list(ctx) {
-  const slides = await fs.readdir(SLIDES_DIR)
-    .catch((error) => {
-      console.error(error);
-      return [];
-    });
+  const slides = await readdir(SLIDES_DIR).catch((error) => {
+    console.error(error);
+    return [];
+  });
 
   ctx.body = slides.filter((slide) => slide.endsWith('.md'));
 }
 
 /**
  * Respond with compiled slide.
- * @param {Koa.Context} ctx 
+ * @param {Koa.Context} ctx
  */
 async function compile(ctx) {
   let compiled = null;
   try {
-    const content = await fs.readFile(join(SLIDES_DIR, ctx.params.slide));
+    const content = await readFile(join(SLIDES_DIR, ctx.params.slide), {
+      encoding: 'utf-8',
+    });
     compiled = await markedWrapper(content.toString());
   } catch (error) {
     console.error(error);
@@ -53,7 +55,7 @@ async function compile(ctx) {
 }
 
 /**
- * 
+ * Promise wrapper for the "marked" module.
  * @param {string} src
  * @returns {Promise<string>}
  */
